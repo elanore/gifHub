02cb87efd7acc4c652476a18bf72ccd5941c28de
elanore <elanorejoseph@gmail.com>
2024-07-29 19:34:08 +0530

    router set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+
+import './App.css';
+
+const router = createBrowserRouter([
+  {
+    element:<AppLayout/>,
+
+    children: [
+      {
+        path:'/',
+        element:<Home/>
+      }
+    ]
+
+  }
+])
 
 function App() {
   const [count, setCount] = useState(0)
