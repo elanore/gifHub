578aebc2fede02fcfbfc269eac458c4b1c673258
elanore <elanorejoseph@gmail.com>
2024-07-31 21:20:19 +0530

    adding header and spme styles

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,13 +1,19 @@
 
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 const AppLayout = () => {
-  return  <div>
-   {/*header placeholder*/ }
-    <main>
-      <Outlet/>
-    </main>
-    </div>;
+  return (
+    <div className="bg-gray-800  text-white min-h-sc">
+      <div className="container px-6 py-4 mx-auto">
+        <Header/>
+
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default AppLayout;
