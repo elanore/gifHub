67ca228215c309e5ebd7f4409ff0139adb4df943
elanore <elanorejoseph@gmail.com>
2024-08-01 22:13:44 +0530

    header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 //import React from 'react'
 
-import { useState } from "react"
+//import { useState } from "react"
 
 const Header = () => {
 
-  const [categories,setCategories]= useState([]);
-  const [showCategories,setshowCategories] =useState(false)
+  //const [categories,setCategories]= useState([]);
+  //const [showCategories,setshowCategories] =useState(false)
   return (
     <div className="header ">Header</div>
   )
