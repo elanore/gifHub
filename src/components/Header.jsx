763df7547642babb21b3e85efb01c66934080c84
elanore <elanorejoseph@gmail.com>
2024-08-01 22:18:57 +0530

    building header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,16 @@
 //import { useState } from "react"
 
 const Header = () => {
-
   //const [categories,setCategories]= useState([]);
   //const [showCategories,setshowCategories] =useState(false)
-  return (
-    <div className="header ">Header</div>
-  )
-}
+  return <nav>
+    <div>
+      <link to="/">
+        <img src="" alt="logo"/>
+        <h1 className="text-lg font-bold cursor-pointer">Giphy</h1>
+      </link>
+    </div>
+  </nav>;
+};
 
-export default Header
+export default Header;
